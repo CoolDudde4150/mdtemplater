540bd8e762ea41b91e524e9c5e95c89550afa2d8
CoolDudde4150 <CoolDudde4150@gmail.com>
2020-03-10 17:59:15 +1030

    DId it again xd

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="Creates a markdown file for note taking",
     author="CoolDudde4150",
     url="https://github.com/CoolDudde4150/mdtemplater",
-    download_url="https://github.com/CoolDudde4150/mdtemplater/archive/v_01.tar.gz",
+    download_url="https://github.com/CoolDudde4150/mdtemplater/archive/v0.1.tar.gz",
     packages=setuptools.find_packages(),
     entry_points={"console_scripts": ["mdtemplater = mdtemplater.templater:main"]},
 )
